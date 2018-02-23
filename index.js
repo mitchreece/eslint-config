9ae8958e12724ca1a7895c78a40d1da50ec3a781
mitchreece <mitchellreece18@gmail.com>
2018-02-23 10:54:38 +0000

    support for node and es6 enviroments

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 module.exports = {
-    root: true,
     parserOptions: {
         ecmaVersion: 6,
+        sourceType: 'module',
+    },
+    env: {
+        es6: true,
+        node: true
     },
     extends: 'eslint:recommended',
     rules: {
